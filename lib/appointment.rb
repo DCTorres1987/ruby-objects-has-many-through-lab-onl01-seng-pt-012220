bed34e84fd8361c672c04cd1614503ab736eb018
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-02-23 16:39:52 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,7 +3,6 @@ require 'pry'
 class Appointment
   attr_accessor :date, :patient, :doctor 
   @@all = []
-https://learn.co/tracks/online-software-engineering-structured/object-oriented-ruby/object-relationships/has-many-objects-through-lab#  
 
   def intialize(date, patient, doctor)
     @date = date 
